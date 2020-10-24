5a2dcded0bc24387b40a93c538cff9e06b3bc4ca
ksiegus <tomek@labutka.pl>
2020-10-24 20:55:56 +0200

    6.6. Pisanie testów JUnit + Mockito

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,10 +1,22 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args){
+        Calculator calc = new Calculator();
+        int resultAdd = calc.add(5, 5);
+        if (resultAdd == 10) {
+            System.out.println("Add test OK");
+        } else {
+            System.out.println("Add test Error!");
+        }
 
+        int resultSubtract = calc.subtract(30, 20);
+        if (resultSubtract == 10) {
+            System.out.println("Subtract test OK");
+        } else {
+            System.out.println("Subtract test Error!");
+        }
     }
 }
